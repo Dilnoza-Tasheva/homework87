a37d6c9a48e00ee9b36a4d97853ec53297bf45e2
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2025-01-24 17:11:38 +0100

    Moved on to the frontend part - added basic page layout and functionality.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import { Container, CssBaseline } from '@mui/material';
+import AppToolbar from './components/UI/AppToolbar/AppToolbar.tsx';
+import { Routes } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <>
+      <CssBaseline/>
+      <header>
+        <AppToolbar/>
+      </header>
+
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+          </Routes>
+        </Container>
+      </main>
 
     </>
   )
---
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,33 @@
+import {AppBar, Button, styled, Toolbar, Typography} from '@mui/material';
+import { Link as NavLink } from 'react-router-dom';
+
+const Link = styled(NavLink)({
+  color: 'inherit',
+  textDecoration: 'none',
+  '&:hover': {
+    color: 'inherit'
+  },
+});
+
+const AppToolbar = () => {
+
+  return (
+    <AppBar position="sticky" sx={{ mb: 2 }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/">Forum</Link>
+        </Typography>
+
+        <Button component={NavLink} to='/register' color="inherit">
+          Register
+        </Button>
+        <Button component={NavLink} to='/register' color="inherit">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppToolbar;
+
